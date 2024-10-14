62d69a229329963ee52e4f3cbc6bf8f84791df47
Saurabh-8585 <khatmodesaurabh7@gmail.com>
2024-10-14 22:27:49 +0530

    minor change in login and changed title

--- src/Pages/Auth/SignIn.jsx
+++ src/Pages/Auth/SignIn.jsx
@@ -2,10 +2,14 @@ import { SignIn } from '@clerk/clerk-react'
 import React from 'react'
 
 const SignInPage = () => {
-    return <SignIn
-        signInFallbackRedirectUrl="/dashboard"
-        signUpFallbackRedirectUrl="/dashboard"
-    />
+    return (
+        <div className='h-screen w-screen flex justify-center items-center'>
+            <SignIn
+                signInFallbackRedirectUrl="/dashboard"
+                signUpFallbackRedirectUrl="/dashboard"
+            />
+        </div>
+    )
 }
 
 export default SignInPage
